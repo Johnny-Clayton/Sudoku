46a711ce0661dad8cdd5b5b5d4c6ec15db3890fb
Johnny <delga300@hotmail.com>
2023-01-17 11:35:52 -0300

    Primeiro commit

---
+++ src/tabuleiro/Sudoku.java
@@ -0,0 +1,96 @@
+package tabuleiro;
+
+import java.util.Scanner;
+
+import board.Board;
+
+public class Sudoku {
+	
+    private int[][] board = new int[9][9];
+    private Scanner scanner = new Scanner(System.in);
+
+    public void initializeBoard() {
+    	
+    	Board b = new Board();
+    	b.initializeBoard();
+    }
+
+    public boolean isValid(int row, int col, int value) {
+
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == value) {
+                return false;
+            }
+        }
+        for (int i = 0; i < 9; i++) {
+            if (board[i][col] == value) {
+                return false;
+            }
+        }
+        int startRow = row - (row % 3);
+        int startCol = col - (col % 3);
+        for (int i = startRow; i < startRow + 3; i++) {
+            for (int j = startCol; j < startCol + 3; j++) {
+                if (board[i][j] == value) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public void play() {
+        while (true) {
+        	clearScreen();
+            displayBoard();
+            System.out.print("Digite a Linha (1-9): ");
+            Integer row = scanner.nextInt() - 1;
+            System.out.print("Digite a Coluna (1-9): ");
+            Integer col = scanner.nextInt() - 1;
+            System.out.print("Digite a Valor (1-9): ");
+            int value = scanner.nextInt();
+            if (isValid(row, col, value)) {
+                board[row][col] = value;
+                if (isComplete()) {
+                    System.out.println("Parabéns! Você resolveu o quebra-cabeça!");
+                    break;
+                }
+            } else {
+                System.out.println("Movimento inválido. Por favor, tente novamente.");
+            }
+        }
+    }
+
+
+    public boolean isComplete() {
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                if (board[row][col] == 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public void displayBoard() {
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                System.out.print(board[row][col] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+    	Sudoku sudoku = new Sudoku();
+        sudoku.initializeBoard();
+        sudoku.play();
+    }
+    
+	public static void clearScreen() {
+		System.out.print("\033[H\033[2J");
+		System.out.flush();
+	}	
+}
+
